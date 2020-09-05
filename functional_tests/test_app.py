c46474202c2a659c16f10ef78a89c36ef52dc641
Tony380 <tonygon2@yahoo.es>
2020-09-05 19:02:38 -0400

    Travis

--- functional_tests/test_app.py
+++ functional_tests/test_app.py
@@ -10,7 +10,6 @@ class TestApp(StaticLiveServerTestCase):
 
     def setUp(self):
         options = Options()
-        options.add_argument('headless')
         options.add_argument('--disable-infobars')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--no-sandbox')
