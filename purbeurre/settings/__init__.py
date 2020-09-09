9b1a70cddf287833ac2ca6fb0fdba702bb7c2385
Tony380 <tonygon2@yahoo.es>
2020-09-09 07:39:16 -0300

    Séparation des environnement

--- purbeurre/settings/__init__.py
+++ purbeurre/settings/__init__.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
